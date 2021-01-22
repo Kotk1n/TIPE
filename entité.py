439c5a941f3b3a1a68b1fef56f0fe225aa76e984
gatienc <73195683+gatienc@users.noreply.github.com>
2021-01-22 10:35:25 +0100

    Add files via upload

---
+++ entité.py
@@ -0,0 +1,30 @@
+import pygame as pg
+
+class Player(pg.sprite.Sprite):
+
+    def __init__(self,x,y,a,b):
+
+        self.velocity = 1.5
+        self.taille = 10
+        self.image = pg.transform.scale(pg.image.load("/home/utilisateur/Bureau/TIPE-Test-fusion-/TIPE/assets/bleu.png"),(self.taille,self.taille))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.arrivé = (a,b)
+        self.compteur = 0
+
+
+
+
+
+    def move_right (self):
+        self.rect.x += self.velocity
+
+    def move_left (self):
+        self.rect.x -= self.velocity
+
+    def move_up(self):
+        self.rect.y -= self.velocity
+    def move_down (self):
+        self.rect.y += self.velocity
+

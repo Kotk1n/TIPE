99853d0c1bba229495bfa6f6737eb4dd60900a40
Thibaut <tibo0206@gmail.com>
2021-03-12 20:32:12 +0100

    début bdd
    création dossier

---
+++ TIPE/bdd.py
@@ -0,0 +1,14 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+create_connection('test.db')

5cdb254aaefaf02a9c8e25f2a2a6f67180d4fd8f
Thibaut <tibo0206@gmail.com>
2021-03-16 13:05:31 +0100

    propre
    propre

--- TIPE/bdd.py
+++ TIPE/bdd.py
@@ -1,20 +1,22 @@
 import sqlite3
 con = sqlite3.connect('test.db')
+cur = con.cursor()
+'''
 def creerbdd():
     cur = con.cursor()
     con.execute(
         "CREATE TABLE evenements (Temps	REAL,particule1	INTEGER,particule2	INTEGER,px	REAL,py	REAL   ")
 
+'''
 
 def remisea0 ():
-    cur = con.cursor()
+
     cur.execute("delete from evenements")
     con.commit()
 
 
 
 def enregistrement(t,p1,p2,x,y):
-    cur=con.cursor()
     cur.execute("insert into evenements values ({},{},{},{},{})".format(t,p1,p2,x,y))
     con.commit()
 
